0e4d16b3fe5375450e949befc3687f9310ae0215
Artem06092005 <chirkin.aleksandrowich@yandex.ru>
2026-04-14 17:51:59 +0700

    update task5

--- module3/task5/main.c
+++ module3/task5/main.c
@@ -6,23 +6,16 @@
 #include <unistd.h>
 
 int fd = -1;
-volatile sig_atomic_t sigint_count = 0;
+volatile sig_atomic_t sigint_flag = 0;
+volatile sig_atomic_t sigquit_flag = 0;
+
+int sigint_count = 0;
 
 void sigHandler(int sig) {
 	if (sig == SIGINT) {
-		sigint_count++;
-		if (sigint_count < 3) {
-			const char msg[] = "\nПолучен сигнал SIGINT\n";
-			write(fd, msg, sizeof(msg) - 1);
-		} else {
-			const char msg[] = "\nПолучен 3-й SIGINT. Завершение...\n";
-			write(fd, msg, sizeof(msg) - 1);
-			close(fd);
-			exit(EXIT_SUCCESS);
-		}
+		sigint_flag = 1;
 	} else if (sig == SIGQUIT) {
-		const char msg[] = "\nПолучен сигнал SIGQUIT\n";
-		write(fd, msg, sizeof(msg) - 1);
+		sigquit_flag = 1;
 	}
 }
 
@@ -38,7 +31,27 @@ int main() {
 	int counter = 1;
 	char buffer[64];
 
+	const char msg1[] = "\nПолучен сигнал SIGINT\n";
+	const char msg2[] = "\nПолучен 3-й SIGINT. Завершение...\n";
+	const char msg3[] = "\nПолучен сигнал SIGQUIT\n";
 	while (1) {
+		if (sigint_flag) {
+			sigint_count++;
+			if (sigint_count < 3) {
+				write(fd, msg1, sizeof(msg1) - 1);
+			} else {
+				write(fd, msg2, sizeof(msg2) - 1);
+				close(fd);
+				exit(EXIT_SUCCESS);
+			}
+			sigint_flag = 0;
+		}
+
+		if (sigquit_flag) {
+			write(fd, msg3, sizeof(msg3) - 1);
+			sigquit_flag = 0;
+		}
+
 		int len = sprintf(buffer, "%d\n", counter++);
 		if (write(fd, buffer, len) == -1) {
 			perror("Error: write()");
