39ece5a318711de6f6169abafa4d3a28020a1bcc
Artem06092005 <chirkin.aleksandrowich@yandex.ru>
2026-04-14 20:56:33 +0700

    update task11

--- module3/task9/main.c
+++ module3/task9/main.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <semaphore.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
@@ -7,12 +8,18 @@
 #include <unistd.h>
 
 char file[] = "file";
-char sem_name[] = "my_sem";
+char sem_name_parent[] = "/sem_p";
+char sem_name_child[] = "/sem_c";
 
 enum { sizeBuff = 32, maxQuantityRand = 50, max_rand = 32768 };
 
+int keep_running = 1;
+void sigHandler(int sg) { keep_running = 0; }
+
 int main() {
-	sem_t* semid = sem_open(sem_name, O_CREAT, 0660, 0);
+	signal(SIGINT, sigHandler);
+	sem_t* sem_p = sem_open(sem_name_parent, O_CREAT, 0660, 1);
+	sem_t* sem_c = sem_open(sem_name_child, O_CREAT, 0660, 0);
 
 	srand(time(NULL));
 	pid_t pid;
@@ -21,53 +28,57 @@ int main() {
 		exit(EXIT_FAILURE);
 
 	} else if (pid == 0) {
-		sem_wait(semid);
-
-		FILE* fd = fopen(file, "r");
-		if (!fd) {
-			perror("fopen()");
-			_exit(EXIT_FAILURE);
-		}
-
-		int num, min, max;
-		int first = 1;
-		while (fscanf(fd, "%d", &num) == 1) {
-			if (first) {
-				min = max = num;
-				first = 0;
-			} else {
-				if (num < min) min = num;
-				if (num > max) max = num;
+		while (1) {
+			sem_wait(sem_c);
+			FILE* fd = fopen(file, "r");
+			if (!fd) {
+				perror("fopen()");
+				_exit(EXIT_FAILURE);
 			}
-		}
-		if (!first) {
-			printf("Min: %d  ; Max: %d\n", min, max);
-		} else {
-			printf("Файл пуст\n");
-		}
-		fclose(fd);
-		sem_post(semid);
-		sem_close(semid);
-		_exit(EXIT_SUCCESS);
 
+			int num, min, max;
+			int first = 1;
+			while (fscanf(fd, "%d", &num) == 1) {
+				if (first) {
+					min = max = num;
+					first = 0;
+				} else {
+					if (num < min) min = num;
+					if (num > max) max = num;
+				}
+			}
+			if (!first) {
+				printf("Min: %d  ; Max: %d\n", min, max);
+			} else {
+				printf("Файл пуст\n");
+			}
+			fclose(fd);
+			sem_post(sem_p);
+		}
 	} else {
-		FILE* fd = fopen(file, "w");
-		if (!fd) {
-			perror("fopen()");
-			_exit(EXIT_FAILURE);
-		}
+		while (keep_running) {
+			sem_wait(sem_p);
+			FILE* fd = fopen(file, "w");
+			if (!fd) {
+				perror("fopen()");
+				_exit(EXIT_FAILURE);
+			}
 
-		int quantityRand = rand() % maxQuantityRand;
-		for (int i = 0; i < quantityRand; i++) {
-			int num = rand() % max_rand - max_rand / 2;
-			fprintf(fd, "%d\n", num);
+			int quantityRand = rand() % maxQuantityRand;
+			for (int i = 0; i < quantityRand; i++) {
+				int num = rand() % max_rand - max_rand / 2;
+				fprintf(fd, "%d\n", num);
+			}
+			fflush(fd);
+			fclose(fd);
+			sem_post(sem_c);
 		}
-		fflush(fd);
-		fclose(fd);
-		sem_post(semid);
-		sem_close(semid);
-		wait(NULL);
-		sem_unlink(sem_name);
+		printf("Конец\n");
+		kill(pid, SIGKILL);
+		sem_close(sem_p);
+		sem_close(sem_c);
+		sem_unlink(sem_name_parent);
+		sem_unlink(sem_name_child);
 		exit(EXIT_SUCCESS);
 	}
 }
